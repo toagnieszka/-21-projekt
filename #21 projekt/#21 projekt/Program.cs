2c1bbf50a1bed10e513476ccb3e8f994c6bad872
Agnieszka Łyczak <agnieszka.lyczak02@gmail.com>
2024-04-05 13:57:57 +0200

    dzień 4

--- #21 projekt/#21 projekt/Program.cs
+++ #21 projekt/#21 projekt/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var name = "Kasia";
+var age = 25;
+var sex = "kobieta";
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == "mezczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
