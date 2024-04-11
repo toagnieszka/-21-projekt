821fb00b986b40a273775a6efc6f61fd8d560b5c
Agnieszka Łyczak <agnieszka.lyczak02@gmail.com>
2024-04-11 16:06:05 +0200

    Dzień 8

---
+++ #21 projekt/#21 projektTests/TypeTests.cs
@@ -0,0 +1,54 @@
+﻿using _21_projekt;
+
+namespace _21_projektTests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void WhenGetEmployeeThenReturnDifferentObjects()
+        {
+            //arrange
+            var employee1 = GetEmployee("Ala", "Nowak", 3);
+            var employee2 = GetEmployee("Kamila", "Kowak", 6);
+
+            //act
+
+
+            //assert
+            Assert.AreNotEqual(employee1, employee2);
+        }
+
+        private Employee GetEmployee(string name, string surname, int age)
+        {
+            return new Employee(name, surname, age);
+        }
+
+        [Test]
+        public void WhenDifferentAgeThenReturnSameResult()
+        {
+            //arrange
+            int age1 = 6;
+            int age2 = 6;
+
+            //act
+
+
+            //assert
+            Assert.AreEqual(age1, age2);
+        }
+
+        [Test]
+        public void WhenDifferentNameThenReturnNotEqualResult()
+        {
+            //arrange
+            string name1 = "Ania";
+            string name2 = "Beata";
+
+            //act
+
+
+            //assert
+            Assert.AreNotEqual(name1, name2);
+        }
+    }
+}
