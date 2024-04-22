aea196dfb692d6e827322dd1171b4a916e451490
Agnieszka Łyczak <agnieszka.lyczak02@gmail.com>
2024-04-22 19:24:48 +0200

    Dzień 15

--- #21 projekt/#21 projekt/Supervisor.cs
+++ #21 projekt/#21 projekt/Supervisor.cs
@@ -2,25 +2,29 @@
 {
     public class Supervisor : IEmployee
     {
+
+        private List <float> grades = new List <float> ();
         public string Name => "Jan";
 
-        public string Surname => throw new NotImplementedException();
+        public string Surname => "Kowalski";
 
-        public string Gender => throw new NotImplementedException();
+        public string Gender => "M";
 
         public void AddGrade(float grade)
         {
-            throw new NotImplementedException();
+            this.grades.Add (grade);
         }
 
         public void AddGrade(int grade)
         {
-            throw new NotImplementedException();
+            float floatValue = (float)grade;
+            this.grades.Add (floatValue);
         }
 
         public void AddGrade(double grade)
         {
-            throw new NotImplementedException();
+            float floatValue = (float)grade;
+            this.grades.Add (floatValue);
         }
 
         public void AddGrade(char grade)
@@ -33,62 +37,62 @@
             switch (grade)
             {
                 case "6":
-                    this.AddGrade("100");
+                    this.AddGrade(100);
                     break;
                 case "-6":
                 case "6-":
-                    this.AddGrade("95");
+                    this.AddGrade(95);
                     break;
                 case "5":
-                    this.AddGrade("80");
+                    this.AddGrade(80);
                     break;
                 case "5+":
                 case "+5":
-                    this.AddGrade("85");
+                    this.AddGrade(85);
                     break;
                 case "-5":
                 case "5-":
-                    this.AddGrade("75");
+                    this.AddGrade(75);
                     break;
                 case "4":
-                    this.AddGrade("60");
+                    this.AddGrade(60);
                     break;
                 case "4+":
                 case "+4":
-                    this.AddGrade("65");
+                    this.AddGrade(65);
                     break;
                 case "-4":
                 case "4-":
-                    this.AddGrade("55");
+                    this.AddGrade(55);
                     break;
                 case "3":
-                    this.AddGrade("40");
+                    this.AddGrade(40);
                     break;
                 case "3+":
                 case "+3":
-                    this.AddGrade("45");
+                    this.AddGrade(45);
                     break;
                 case "-3":
                 case "3-":
-                    this.AddGrade("35");
+                    this.AddGrade(35);
                     break;
                 case "2":
-                    this.AddGrade("20");
+                    this.AddGrade(20);
                     break;
                 case "2+":
                 case "+2":
-                    this.AddGrade("25");
+                    this.AddGrade(25);
                     break;
                 case "-2":
                 case "2-":
-                    this.AddGrade("15");
+                    this.AddGrade(15);
                     break;
                 case "1":
-                    this.AddGrade("0");
+                    this.AddGrade(0);
                     break;
                 case "1+":
                 case "+1":
-                    this.AddGrade("5");
+                    this.AddGrade(5);
                     break;
                 default:
                     throw new Exception("Wrong Number");
@@ -97,7 +101,41 @@
 
         public Statistics GetStatistics()
         {
-            throw new NotImplementedException();
+            {
+                var statistics = new Statistics();
+                statistics.Max = float.MinValue;
+                statistics.Min = float.MaxValue;
+                statistics.Average = 0;
+
+                foreach (var grade in grades)
+                {
+                    statistics.Max = Math.Max(statistics.Max, grade);
+                    statistics.Min = Math.Min(statistics.Min, grade);
+                    statistics.Average += grade;
+                }
+
+                statistics.Average /= this.grades.Count;
+
+                switch (statistics.Average)
+                {
+                    case var average when average > 80:
+                        statistics.AverageLetter = 'A';
+                        break;
+                    case var average when average > 60:
+                        statistics.AverageLetter = 'B';
+                        break;
+                    case var average when average > 40:
+                        statistics.AverageLetter = 'C';
+                        break;
+                    case var average when average > 20:
+                        statistics.AverageLetter = 'D';
+                        break;
+                    default:
+                        statistics.AverageLetter = 'E';
+                        break;
+                }
+                return statistics;
+            }
         }
     }
 }
