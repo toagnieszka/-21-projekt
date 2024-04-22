d0aa55d9ce5db8232c366fb03faec9f0bb7c7f13
Agnieszka Łyczak <agnieszka.lyczak02@gmail.com>
2024-04-22 21:13:09 +0200

    Dzień 15

--- #21 projekt/#21 projekt/Supervisor.cs
+++ #21 projekt/#21 projekt/Supervisor.cs
@@ -29,32 +29,9 @@
 
         public void AddGrade(char grade)
         {
+            float floatValue = (float)grade;
+            this.AddGrade(floatValue);
 
-            switch (grade)
-            {
-                case 'A':
-                case 'a':
-                    this.grades.Add(100);
-                    break;
-                case 'B':
-                case 'b':
-                    this.grades.Add(80);
-                    break;
-                case 'C':
-                case 'c':
-                    this.grades.Add(60);
-                    break;
-                case 'D':
-                case 'd':
-                    this.grades.Add(40);
-                    break;
-                case 'E':
-                case 'e':
-                    this.grades.Add(20);
-                    break;
-                default:
-                    throw new Exception("Wrong letter");
-            }
         }
 
         public void AddGrade(string grade)
